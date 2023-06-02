ddd040a0ca3be7b643065db7a66ae585ea9e8a27
TechiAkki963 <57339768+TechiAkki963@users.noreply.github.com>
2023-06-03 04:07:27 +0530

    Update script.js
    final

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
+"use strict";
 const textarea = document.querySelector("textarea"),
-voiceList = document.querySelector("Select"),
-      speechBtn = document.querySelector("button");
+  voiceList = document.querySelector("Select"),
+  speechBtn = document.querySelector("button");
 
 let synth = speechSynthesis,
-    isSpeaking= true;
+  isSpeaking = true;
 
 voices();
 
-function voices(){
-for (let voice of synth.getVoices()){
-let selected= voice.name === "Google US English"?"selected":"";
-  let option=`<option value="${voice.name}"${selected}>
-  ${voice.name}(${voice.lang})</option>`;
-  voiceList.insertAdjacentHTML("beforeend",option);
-}
-}
-
-synth.addEventListener("voicechanged",voices);
-
-function textToSpeech(text){
-let utterance =new SpeechSynthesisisUtterance(text);
-  for(let voice of synth.getVoices()){
-  if(voice.name ===voiceList.value){
-  utterance.voice=voice;
+function voices() {
+  for (let voice of synth.getVoices()) {
+    let selected = voice.name === "Google US English" ? "selected" : "";
+    let option = `<option value="${voice.name}" ${selected}>
+      ${voice.name}(${voice.lang})</option>`;
+    voiceList.insertAdjacentHTML("beforeend", option);
   }
+}
+
+synth.addEventListener("voicechanged", voices);
+
+function textToSpeech(text) {
+  let utterance = new SpeechSynthesisUtterance(text);
+  for (let voice of synth.getVoices()) {
+    if (voice.name === voiceList.value) {
+      utterance.voice = voice;
+    }
   }
   synth.speak(utterance);
 }
+
+speechBtn.addEventListener("click", function() {
+  textToSpeech(textarea.value);
+});
